3d8fc348f3e3e034418513aa5dca8aec0020f0c2
julianv97 <julian.vicente@radiumrocket.com>
2022-02-13 20:24:22 -0300

    IC: refactor getData helper

--- src/helpers/asyncStorage/index.ts
+++ src/helpers/asyncStorage/index.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const getData = async () => {
+export const getData = async (item: string) => {
   try {
-    const value = await AsyncStorage.getItem('@storage_Key');
+    const value = await AsyncStorage.getItem(item);
     if (value !== null) {
       return value;
     }
