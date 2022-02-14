7e18f765e0266136ff6f14b39732b6fc23bd0ed7
julianv97 <julian.vicente@radiumrocket.com>
2022-02-13 23:42:37 -0300

    IC: add constants for actions

--- src/redux/auth/actions.ts
+++ src/redux/auth/actions.ts
@@ -1,14 +1,15 @@
 import firebase from 'firebase/compat';
 import {IRegisterData} from '../../interfaces';
+import {SET_REGISTER_USER, SET_CURRENT_USER} from './constants';
 
 export const setCurrentUser = (
   data: firebase.firestore.DocumentData | undefined,
 ) => ({
-  type: 'SET_CURRENT_USER',
+  type: SET_CURRENT_USER,
   payload: data,
 });
 
 export const setRegisterUser = (data: IRegisterData) => ({
-  type: 'SET_REGISTER_USER',
+  type: SET_REGISTER_USER,
   payload: data,
 });
--- src/redux/auth/reducer.ts
+++ src/redux/auth/reducer.ts
@@ -1,13 +1,20 @@
+import {Action} from '../../types/redux';
+import {SET_REGISTER_USER, SET_CURRENT_USER} from './constants';
+
 const initialState = {
   currentUser: null,
 };
 
-const authReducer = (state = initialState, action) => {
+const authReducer = (state = initialState, action: Action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
+      };
+    case SET_REGISTER_USER:
+      return {
+        ...state,
       };
     default:
       return state;
