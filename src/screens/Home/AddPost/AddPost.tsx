c634d58820c5fe17c06409e097d46c206db00c12
julianv97 <julian.vicente@radiumrocket.com>
2022-02-21 22:28:54 -0300

    IC: create camera component

--- src/screens/Home/AddPost/AddPost.tsx
+++ src/screens/Home/AddPost/AddPost.tsx
@@ -1,10 +1,41 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import {View, TouchableWithoutFeedback, Image} from 'react-native';
+import {RNCamera, TakePictureOptions} from 'react-native-camera';
+import styles from './styles';
 
-const AddPost = () => {
+const AddPost: React.FC = () => {
+  const camRef = useRef<any>(null);
+  const [isCapturing, setIsCapturing] = useState(false);
+  const [image, setSimage] = useState(null);
+
+  const onPressHandler = async () => {
+    if (camRef && !isCapturing) {
+      setIsCapturing(true);
+      const options: TakePictureOptions = {
+        quality: 1,
+        base64: true,
+        width: 2000,
+      };
+      const data = await camRef.current.takePictureAsync(options);
+      setSimage(data.uri);
+    }
+  };
+
   return (
-    <View>
-      <Text>Add Post</Text>
+    <View style={styles.container}>
+      <RNCamera ref={camRef} style={styles.preview} captureAudio={false}>
+        <TouchableWithoutFeedback onPress={onPressHandler}>
+          <View style={styles.button} />
+        </TouchableWithoutFeedback>
+      </RNCamera>
+      {image && (
+        <Image
+          source={{uri: image}}
+          style={{
+            flex: 1,
+          }}
+        />
+      )}
     </View>
   );
 };
---
+++ src/screens/Home/AddPost/styles.ts
@@ -0,0 +1,37 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  button: {
+    width: 72,
+    height: 72,
+    borderRadius: 72 / 2,
+    backgroundColor: 'white',
+    position: 'absolute',
+    bottom: 92,
+    alignSelf: 'center',
+  },
+  container: {
+    flex: 1,
+  },
+  spacing: {
+    position: 'absolute',
+    bottom: 140,
+  },
+  title: {
+    position: 'absolute',
+    top: 100,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  backArrow: {
+    position: 'absolute',
+    top: 5,
+    left: 21,
+    zIndex: 100,
+  },
+});
+
+export default styles;
