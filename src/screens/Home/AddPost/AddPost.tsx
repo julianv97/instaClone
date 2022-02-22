47ff74dd12e5db8b6f047eae73597c4dc8fd001c
julianv97 <julian.vicente@radiumrocket.com>
2022-02-21 23:16:39 -0300

    IC: add comments

--- src/screens/Home/AddPost/AddPost.tsx
+++ src/screens/Home/AddPost/AddPost.tsx
@@ -2,16 +2,22 @@ import React, {useState, useRef} from 'react';
 import {View, TouchableWithoutFeedback, Image, Button} from 'react-native';
 import {RNCamera, TakePictureOptions} from 'react-native-camera';
 import {launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'react-native-image-picker';
 import styles from './styles';
 
-const pickerOptions = {
-  title: 'Select Image',
-  customButtons: [
-    {name: 'customOptionKey', title: 'Choose Photo from Custom Option'},
-  ],
-  storageOptions: {
-    skipBackup: true,
-    path: 'images',
+interface Action {
+  title: string;
+  type: 'capture' | 'library';
+  options: ImagePicker.CameraOptions | ImagePicker.ImageLibraryOptions;
+}
+
+const pickerOptions: Action = {
+  title: 'Select Image or Video\n(mixed)',
+  type: 'library',
+  options: {
+    // Con selectionLimit se puede setear cuantas fotos elegir, si pongo 0 puedo elegir cualquier cantidad
+    selectionLimit: 1,
+    mediaType: 'mixed',
   },
 };
 
@@ -20,12 +26,16 @@ const AddPost: React.FC = () => {
   const [isCapturing, setIsCapturing] = useState<boolean>(false);
   const [image, setSimage] = useState<string | undefined>(undefined);
 
-  const buttonPress = async () => {
-    const result = await launchImageLibrary(pickerOptions?.storageOptions);
-    setSimage(result?.assets[0]?.uri);
+  const imagePickerHandler = async () => {
+    const result = await launchImageLibrary(pickerOptions?.options);
+    // Si selecciono mas de una imágen, assets es un array con las imágenes seleccionadas
+    // Como selecciono solo una, assets es un array con una sola posición
+    if (result.assets?.length) {
+      setSimage(result.assets[0].uri);
+    }
   };
 
-  const onPressHandler = async () => {
+  const onTakePhotoHandler = async () => {
     if (camRef && !isCapturing) {
       setIsCapturing(true);
       const options: TakePictureOptions = {
@@ -41,12 +51,12 @@ const AddPost: React.FC = () => {
   return (
     <View style={styles.container}>
       <RNCamera ref={camRef} style={styles.preview} captureAudio={false}>
-        <TouchableWithoutFeedback onPress={onPressHandler}>
+        <TouchableWithoutFeedback onPress={onTakePhotoHandler}>
           <View style={styles.button} />
         </TouchableWithoutFeedback>
       </RNCamera>
       {image && <Image source={{uri: image}} style={styles.image} />}
-      <Button title="Pick from your gallery" onPress={buttonPress} />
+      <Button title="Pick from your gallery" onPress={imagePickerHandler} />
     </View>
   );
 };
