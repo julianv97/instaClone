e7b32287673ed2a2caab49f25240628cd46a406d
julianv97 <julian.vicente@radiumrocket.com>
2022-02-26 23:34:22 -0300

    IC: register fields updated

--- src/screens/Auth/Register/Register.tsx
+++ src/screens/Auth/Register/Register.tsx
@@ -1,11 +1,12 @@
-import {View, Text, TextInput, Button} from 'react-native';
+import {View, Text, Button} from 'react-native';
 import React from 'react';
-import {useForm, Controller} from 'react-hook-form';
+import {useForm} from 'react-hook-form';
 import {registerUser} from '../../../redux/auth/thunks';
 import {useDispatch} from 'react-redux';
 import {IRegisterData} from '../../../interfaces';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../../../types/navigation';
+import Field from '../../../components/Field/Field';
 
 interface Props {
   navigation: NativeStackNavigationProp<RootStackParamList, 'Register'>;
@@ -33,63 +34,42 @@ const Register: React.FC<Props> = ({navigation}) => {
   return (
     <View>
       <Text>Register</Text>
-      <Controller
+
+      <Field
         control={control}
-        rules={{
-          required: true,
-        }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            placeholder="name"
-            textContentType="name"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
         name="name"
-      />
-
-      {errors.name && <Text>This is required.</Text>}
-
-      <Controller
-        control={control}
+        placeholder="name"
         rules={{
           required: true,
         }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            placeholder="email"
-            textContentType="emailAddress"
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
+        error={errors.name}
+        errorMessage="Name is required"
+      />
+
+      <Field
+        control={control}
         name="email"
-      />
-
-      {errors.email && <Text>This is required.</Text>}
-
-      <Controller
-        control={control}
+        placeholder="email"
         rules={{
           required: true,
         }}
-        render={({field: {onChange, onBlur, value}}) => (
-          <TextInput
-            placeholder="password"
-            textContentType="password"
-            secureTextEntry={true}
-            onBlur={onBlur}
-            onChangeText={onChange}
-            value={value}
-          />
-        )}
-        name="password"
+        error={errors.email}
+        errorMessage="Email is required"
+        textContentType={'emailAddress'}
       />
 
-      {errors.password && <Text>This is required.</Text>}
+      <Field
+        control={control}
+        name="password"
+        placeholder="password"
+        rules={{
+          required: true,
+        }}
+        error={errors.password}
+        errorMessage="Password is required"
+        textContentType={'password'}
+        secureTextEntry={true}
+      />
 
       <Button title="Sign Up" onPress={handleSubmit(onSubmit)} />
     </View>
