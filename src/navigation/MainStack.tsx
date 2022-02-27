fb7095def8cc36d2c6054d2df34a74e5717d6d3a
julianv97 <julian.vicente@radiumrocket.com>
2022-02-27 16:15:09 -0300

    IC: update redirect when user is logged

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -3,30 +3,24 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from '@customTypes/navigation';
 import {useSelector} from 'react-redux';
+import {RootState} from '@redux/index';
 import Landing from '@screens/Auth/Landing/Landing';
 import Register from '@screens/Auth/Register/Register';
 import Login from '@screens/Auth/Login/Login';
 import Home from '@screens/Home/Home/Home';
 import AddPost from '@screens/Home/AddPost/AddPost';
 import SavePost from '@screens/Home/SavePost/SavePost';
-import {RootState} from '@redux/index';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-// TODO: verificar cuando el usuario ya está logueado y redirigir a Home
-
 const MainStack: React.FC = () => {
-  const user = useSelector<RootState>(state => state.auth.currentUser);
   const authenticated = useSelector<RootState>(
     state => state.auth.authenticated,
   );
 
-  console.log('MainStack: user', user);
-  console.log('MainStack: authenticated', authenticated);
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Landing">
+      <Stack.Navigator initialRouteName={authenticated ? 'Home' : 'Landing'}>
         <Stack.Screen name="Landing" component={Landing} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
