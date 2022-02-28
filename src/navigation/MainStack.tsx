b88b242848d4d467498d228098baad6095c6bdf7
julianv97 <julian.vicente@radiumrocket.com>
2022-02-28 13:51:05 -0300

    IC: remove constants

--- src/navigation/MainStack.tsx
+++ src/navigation/MainStack.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RootStackParamList} from '@customTypes/navigation';
-import {useSelector} from 'react-redux';
-import {RootState} from '@redux/index';
 import Landing from '@screens/Auth/Landing/Landing';
 import Register from '@screens/Auth/Register/Register';
 import Login from '@screens/Auth/Login/Login';
@@ -14,13 +12,9 @@ import SavePost from '@screens/Home/SavePost/SavePost';
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 const MainStack: React.FC = () => {
-  const authenticated = useSelector<RootState>(
-    state => state.auth.authenticated,
-  );
-
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={authenticated ? 'Home' : 'Landing'}>
+      <Stack.Navigator initialRouteName={'Landing'}>
         <Stack.Screen name="Landing" component={Landing} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="Login" component={Login} />
