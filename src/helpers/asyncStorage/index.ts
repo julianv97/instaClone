c2201dfa68bb275df11a73b562747a8557f5ac82
julianv97 <julian.vicente@radiumrocket.com>
2022-02-28 13:54:58 -0300

    IC: create setter AS helper

--- src/helpers/asyncStorage/index.ts
+++ src/helpers/asyncStorage/index.ts
@@ -1,11 +1,19 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const getData = async (item: string) => {
+export const getDataAsyncStorage = async (item: string) => {
   try {
     const value = await AsyncStorage.getItem(item);
     if (value !== null) {
       return value;
     }
+  } catch {
+    return undefined;
+  }
+};
+
+export const setDataAsyncStorage = async (item: string, value: any) => {
+  try {
+    await AsyncStorage.setItem(item, value);
   } catch {
     return undefined;
   }
